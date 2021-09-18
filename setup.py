e8580177564cebe52d828fdc54c1e9f484909516
tomasrajao <tomasrajao@gmail.com>
2021-09-18 14:20:39 -0300

    Corrigida variável PACKAGE do setup.py

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro2"
+PACKAGE = "libpythonpro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Tomas Rajao"
