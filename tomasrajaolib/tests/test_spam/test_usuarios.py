3d5c31157ac344992e2b5c0c0af010532a0619e7
tomasrajao <tomasrajao@gmail.com>
2021-09-21 13:16:21 -0300

    Teste usuarios

---
+++ tomasrajaolib/tests/test_spam/test_usuarios.py
@@ -0,0 +1,16 @@
+from tomasrajaolib.spam.modelos import Usuario
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuarios(sessao):
+    usuarios = [
+        Usuario(nome='Renzo', email='renzo@python.pro.br'),
+        Usuario(nome='Luciano', email='luciano@python.pro.br'),
+    ]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
